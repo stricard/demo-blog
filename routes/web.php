ca3f20524649abb3aa8c79c913f56838de58a290
Sylvain Tricard <sylvain.tricard@protection24.com>
2021-09-05 21:57:52 +0200

    Ajout des urls de l'app

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('layout');
-});
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'home']);
+Route::post('/users', [\App\Http\Controllers\UserController::class, 'store']);
+
+Route::get('/articles', [\App\Http\Controllers\ArticlesController::class, 'liste']);
