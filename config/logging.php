78739267a70d4e2934c9aafa0b4d461e54b3e8b9
Sylvain Tricard <sylvain.tricard@protection24.com>
2021-09-05 20:44:55 +0200

    Création d'un middleware pour logguer les API
    log seulement les API dans un fichier à part

--- config/logging.php
+++ config/logging.php
@@ -47,6 +47,12 @@ return [
             'level' => env('LOG_LEVEL', 'debug'),
         ],
 
+        'APIlog' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/log-api.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+        ],
+
         'daily' => [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
