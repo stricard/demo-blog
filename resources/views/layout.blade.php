d92ef45c373ee86cc0d8987770288dd30bd43467
Sylvain Tricard <sylvain.tricard@protection24.com>
2021-09-06 22:43:04 +0200

    maj layout principal

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -22,7 +22,7 @@
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Demo Blog - Test Zelty</a>
+                <a class="navbar-brand" href="./">Demo Blog - Test Zelty</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -42,8 +42,11 @@
     </main>
 
     <footer class="footer">
-        <div class="container">
+        <div class="container text-center">
+            <br><br>
+            <hr>
             <span class="text-muted">© 2018 Sylvain Tricard</span>
+            <br><br>
         </div>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
