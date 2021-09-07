29285d87ddf94396a508016c810fdf37a4c2c57a
Sylvain Tricard <sylvain.tricard@protection24.com>
2021-09-07 18:26:06 +0200

    update api request

--- app/Http/Requests/API/StoreUserRequest.php
+++ app/Http/Requests/API/StoreUserRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\API;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreUserRequest extends FormRequest
+class StoreUserRequest extends ApiRequest
 {
     /**
      * Determine if the user is authorized to make this request.
--- app/Http/Requests/API/UpdateArticleRequest.php
+++ app/Http/Requests/API/UpdateArticleRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\API;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateArticleRequest extends FormRequest
+class UpdateArticleRequest extends ApiRequest
 {
     /**
      * Determine if the user is authorized to make this request.
