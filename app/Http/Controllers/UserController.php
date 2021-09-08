668d755c1335faa711b3de548c91281bfba1b9ba
Sylvain Tricard <sylvain.tricard@protection24.com>
2021-09-08 08:24:55 +0200

    nettoyage controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,24 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
-use Facade\FlareClient\Api;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Http;
 
 class UserController extends Controller
 {
     public function store(Request $request)
     {
-        $name = $request['name'] ?? null;
-        $email = $request['email'] ?? null;
-        //echo $name . ' - ' . $email;
-
-        $userRequest = Request::create('api/users', 'POST', [
-            'name' => $name,
-            'email' => $email
-        ]);
-        $result = json_decode(Route::dispatch($userRequest)->getContent());
-        dd($result);
+        //à voir si nécessaire
     }
 }
