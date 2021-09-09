3a248d194f2d7ae96c30e812258b9e18a514a117
Sylvain Tricard <sylvain.tricard@protection24.com>
2021-09-09 22:41:20 +0200

    fix test

--- automation/php/dependencies.sh
+++ automation/php/dependencies.sh
@@ -11,3 +11,16 @@ apk add --no-progress --no-cache --upgrade zip unzip git curl
 
 # Installation de composer
 curl --silent --show-error https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+# Installation xdebug pour permettre la génération des rapports de couverture des tests
+pecl install xdebug-2.9.2 && \
+docker-php-ext-enable xdebug && \
+
+apk del .build-deps && \
+rm -r /tmp/pear/* && \
+echo -e "xdebug.remote_enable=1\n\
+        xdebug.remote_autostart=1\n\
+        xdebug.remote_connect_back=0\n\
+        xdebug.remote_port=9001\n\
+        xdebug.remote_log=/var/www/html/xdebug.log\n\
+        xdebug.remote_host=host.docker.internal" >>/usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
