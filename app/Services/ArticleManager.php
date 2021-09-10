dcdb36e476d73edd5d49f7c56f7ded4c4f96647d
Sylvain Tricard <sylvain.tricard@protection24.com>
2021-09-10 08:34:43 +0200

    modification update articles

--- app/Services/ArticleManager.php
+++ app/Services/ArticleManager.php
@@ -19,10 +19,6 @@ class ArticleManager
 
         //on créé notre objet article
         $article = new Article($data_article);
-        /*$article->title = $data_article['title'];
-        $article->contents = $data_article['contents'];
-        $article->user_id = $data_article['user_id'];
-        $article->status_id = $data_article['status_id'];*/
 
         //on récupère les différents status d'un article
         $articleStatus = ArticleStatus::find($article->status_id);
@@ -40,24 +36,27 @@ class ArticleManager
 
     public function updateArticle(array $data_article, Article $article)
     {
+        if (!empty($data_article['title']) && $data_article['title'] !== $article->title){
+            //on vérifie qu'il n'y a pas d'article avec le même titre
+            $existingArticle = Article::query()->where('title', $data_article['title'])->first();
+
+            if (!empty($existingArticle))
+                throw new ArticleAlreadyExists($data_article['title']);
+        }
+
+        $InitialStatusId = $article->status_id ?? null;
         //on modifie notre objet article
         $article->title = $data_article['title'] ?? $article->title;
         $article->contents = $data_article['contents'] ?? $article->contents;
         $article->status_id = $data_article['status_id'] ?? $article->status_id;
-        $article->publicated_at = $data_article['publicated_at'] ?? $article->publicated_at;
-        //dd($data_article);
-        $InitialStatusId = $article->status_id ?? null;
 
-        if ($InitialStatusId !== $data_article['status_id']) {
-            //on récupère les différents status d'un article
-            $articleStatus = ArticleStatus::find($data_article['status_id']);
-
-            //gestion de la date de publication en fonction du status de l'article
-            if ($articleStatus->code === 'PUBLIE')
-                $article->publicated_at = now();
-            else if ($articleStatus->code === 'BROUILLON')
-                $article->publicated_at = $data_article['publicated_at'] ?? null;
-        }
+        //gestion du status
+        $articleStatus = ArticleStatus::find($article->status_id);
+        $articleInitialStatus = ArticleStatus::find($InitialStatusId);
+        if (($articleStatus->code !== $articleInitialStatus->code) && $articleStatus->code === 'PUBLIE')
+            $article->publicated_at = now();
+        else if ($articleStatus->code === 'BROUILLON')
+            $article->publicated_at = $data_article['publicated_at'] ?? $article->publicated_at;
 
         $article->save();
 
